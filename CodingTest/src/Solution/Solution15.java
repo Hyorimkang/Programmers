3bc1c7d6422d226549ed569c8bad23eaafc13ea2
HyorimKang <s2219@e-mirim.hs.kr>
2023-06-17 14:06:10 +0900

    핸드폰 번호 가리기

---
+++ CodingTest/src/Solution/Solution15.java
@@ -0,0 +1,16 @@
+package Solution;
+
+import java.util.ArrayList;
+
+public class Solution15 {
+	 public String solution(String phone_number) {
+	        String answer = "";
+	        String arr[] = phone_number.split("");
+	        
+	        for(int i = 0; i<arr.length-4; i++){
+	            arr[i]="*";
+	        }
+	        answer = String.join("", arr);
+	        return answer;
+	 }
+}
