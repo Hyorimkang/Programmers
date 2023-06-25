bbb1ebedb38342913a8b282f0143dea3ccbe6223
HyorimKang <s2219@e-mirim.hs.kr>
2023-06-25 22:33:41 +0900

    음양더하기

---
+++ CodingTest/src/Solution/Solution17.java
@@ -0,0 +1,17 @@
+package Solution;
+
+public class Solution17 {
+	public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i<signs.length; i++){
+            if(signs[i] == false){
+                answer -= absolutes[i];
+            }
+            else{
+                answer += absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
