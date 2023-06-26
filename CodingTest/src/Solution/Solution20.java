03d13f6fcc551906e4ec493bd6e5b857ef5f3dcd
HyorimKang <s2219@e-mirim.hs.kr>
2023-06-27 08:45:33 +0900

    수박수박수박수박수?

---
+++ CodingTest/src/Solution/Solution20.java
@@ -0,0 +1,16 @@
+package Solution;
+
+public class Solution20 {
+	public String solution(int n) {
+        String answer = "";
+        for(int i = 1; i<=n; i++){
+            if(i%2!=0){
+                answer += "수";
+            }
+            else{
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
